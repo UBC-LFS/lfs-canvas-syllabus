bb6f307ade4f8d13e83c4bb8891cccb1b7c54111
Justin Lee <justinlee0022@gmail.com>
2018-06-14 14:12:20 -0700

    Set up complete

--- index.js
+++ index.js
@@ -1,9 +1,8 @@
-const { recipes } = require('node-canvas-api')
+const cAPI = require('node-canvas-api')
 
 const year = 2017
 const term = 'W1'
 
 ;(async function () {
-  const syllabi = await recipes.getAllCourseSyllabiInTerm(15, year, term)
-  console.log(syllabi)
+  
 })()
