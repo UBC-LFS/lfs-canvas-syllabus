a2ed0bb12eb065a054f724e535875ec6b3c5bc7d
Justin Lee <justinlee0022@gmail.com>
2018-06-19 15:29:22 -0700

    Added test

---
+++ test/cleanHTML.test.js
@@ -0,0 +1,18 @@
+const removeNewline = require('../src/util/cleanHTML')
+const assert = require('assert')
+
+describe('removeNewline', () => {
+  it('takes in HTML string and removes `\n`', () => {
+    let input = '<p>\n</p>'
+    assert.deepEqual(removeNewline(input), '<p></p>')
+
+    input = '<p>\nhello</p>'
+    assert.deepEqual(removeNewline(input), '<p>hello</p>')
+
+    input = '<p></p>'
+    assert.deepEqual(removeNewline(input), '<p></p>')
+
+    input = '<p>\n\n</p>\n'
+    assert.deepEqual(removeNewline(input), '<p></p>')
+  })
+})
