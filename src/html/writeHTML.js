6b8d505cd707804d7dde15974ea5a3bd44d33276
Justin Lee <justinlee0022@gmail.com>
2018-06-22 15:47:04 -0700

    Modified writeHTML

--- src/html/writeHTML.js
+++ src/html/writeHTML.js
@@ -3,10 +3,10 @@ const { promisify } = require('util')
 const path = require('path')
 const fsWrite = promisify(fs.writeFile)
 
-const createFilePath = (term, name, course) => path.join(__dirname, `/output/${term}-${name}-${course}.html`)
+const createFilePath = (term, course) => path.join(__dirname, `/output/${term}-${course}.html`)
 
 const writeHTML = ({ html, term, name, course }) => {
-  const filePath = createFilePath(term, name, course)
+  const filePath = createFilePath(term, course)
   try {
     fsWrite(filePath, html)
   } catch (e) {
