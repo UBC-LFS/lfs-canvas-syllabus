ce61a4f4918cb075449065fef5719746244eae6b
Justin Lee <justinlee0022@gmail.com>
2018-06-29 12:19:37 -0700

    Updated entry point

--- src/server/app.js
+++ src/server/app.js
@@ -8,9 +8,6 @@ const app = express()
 app.use(express.static(path.join(__dirname, '../public/build')))
 app.disable('x-powered-by')
 
-app.use(logger('dev', {
-  skip: () => app.get('env') === 'test'
-}))
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
