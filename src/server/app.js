8649e1c7c1a87abd52e68eadc3071ee013a646c0
Justin Lee <justinlee0022@gmail.com>
2018-07-03 10:30:29 -0700

    Added react app for client

--- src/server/app.js
+++ src/server/app.js
@@ -12,6 +12,12 @@ app.disable('x-powered-by')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
+app.use((req, res, next) => {
+  res.header('Access-Control-Allow-Origin', '*')
+  res.header('Access-Control-Allow-Headers', 'Origin, X-Requested-With, Content-Type, Accept')
+  next()
+})
+
 // Routes
 app.use('/', routes)
 
--- src/server/static/js/index.js
+++
@@ -1,6 +0,0 @@
-/* global fetch */
-
-document.addEventListener('DOMContentLoaded', async function () {
-  const terms = await fetch('http://localhost:8080/terms').then(res => res.json())
-  console.log(terms)
-}, false)
