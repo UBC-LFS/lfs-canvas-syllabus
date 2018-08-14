65404c65072d39803b4a00586d2d3318f8f611d6
Justin Lee <justinlee0022@gmail.com>
2018-08-14 08:21:39 -0700

    sending list of courses and terms

--- src/server/routes.js
+++ src/server/routes.js
@@ -1,22 +1,33 @@
 const { Router } = require('express')
 const path = require('path')
-// require('dotenv').config()
 const readDirs = require('./util/readDirs')
+const { flatten } = require('ramda')
 
 const routes = Router()
 
-routes.get('/terms', async (req, res) => {
-  const dirs = await readDirs()
-  res.send(dirs)
-})
-
 routes.get('/courses/:term', async ({ params: { term } }, res) => {
-  const dirs = await readDirs(term)
-  res.send(dirs)
+  const coursesInTerm = await readDirs(term)
+  res.send(coursesInTerm)
 })
 
 routes.get('/syllabi/:term/:course', ({ params: { term, course } }, res) => {
   res.sendFile(path.join(__dirname, `../../output/syllabi/${term}/${course}/index.html`))
+})
+
+routes.get('/courses', async (req, res) => {
+  const terms = await readDirs()
+  const courses = flatten(
+    await Promise.all(
+      terms.map(term => {
+        return readDirs(term)
+          .then(courses => ({
+            courses,
+            term
+          }))
+      })
+    )
+  )
+  res.send(courses)
 })
 
 module.exports = routes
