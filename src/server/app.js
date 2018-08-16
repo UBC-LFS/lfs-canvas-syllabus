49d4a88d6d500aef53bc8c6851230a99be126c4a
Justin Lee <justinlee0022@gmail.com>
2018-08-16 15:26:27 -0700

    Added back error handling

--- src/server/app.js
+++ src/server/app.js
@@ -19,4 +19,20 @@ app.disable('x-powered-by')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
+// Catch 404 and forward to error handler
+app.use((req, res, next) => {
+  const err = new Error('Not Found')
+  err.status = 404
+  next(err)
+})
+
+// Error handler
+app.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
+  res
+    .status(err.status || 500)
+    // .render('error', {
+    //   message: err.message
+    // });
+})
+
 module.exports = app
