03fa1a60a3e5db4abf83dd76c235100e8c6299c2
Justin Lee <justinlee0022@gmail.com>
2018-08-16 15:20:10 -0700

    Added redirect

--- src/server/app.js
+++ src/server/app.js
@@ -19,20 +19,8 @@ app.disable('x-powered-by')
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
-// Catch 404 and forward to error handler
-app.use((req, res, next) => {
-  const err = new Error('Not Found')
-  err.status = 404
-  next(err)
-})
-
-// Error handler
-app.use((err, req, res, next) => { // eslint-disable-line no-unused-vars
-  res
-    .status(err.status || 500)
-    // .render('error', {
-    //   message: err.message
-    // });
+app.get('*', (req, res) => {
+  res.redirect('http://prod-lc01-pub.landfood.ubc.ca/lfscourses/')
 })
 
 module.exports = app
