b0ab26e85aee8fb9fa61c09901eefab420aa4abf
Justin Lee <justinlee0022@gmail.com>
2018-08-16 15:55:18 -0700

    removed static

--- src/server/app.js
+++ src/server/app.js
@@ -16,8 +16,6 @@ app.use('/', routes)
 
 app.disable('x-powered-by')
 
-app.use('/', express.static(path.join(__dirname, '/static/')))
-
 app.use(bodyParser.json())
 app.use(bodyParser.urlencoded({ extended: true }))
 
