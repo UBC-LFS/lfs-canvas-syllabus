61d06c80a554e0048f51e37e4e183101dadffd6d
Justin Lee <justinlee0022@gmail.com>
2018-08-16 14:44:04 -0700

    Added logs

--- src/server/routes.js
+++ src/server/routes.js
@@ -11,23 +11,8 @@ routes.get('/lfssyllabi/courses/:term', async ({ params: { term } }, res) => {
 })
 
 routes.get('/lfssyllabi/syllabi/:term/:course', ({ params: { term, course } }, res) => {
+  console.log('hit here')
   res.sendFile(path.join(__dirname, `../../output/syllabi/${term}/${course}/index.html`))
-})
-
-routes.get('/availableSyllabi', async (req, res) => {
-  const terms = await readDirs()
-  const courses = flatten(
-    await Promise.all(
-      terms.map(term => {
-        return readDirs(term)
-          .then(courses => ({
-            courses: courses.map(course => course.split('.')[0]),
-            term
-          }))
-      })
-    )
-  )
-  res.send(courses)
 })
 
 module.exports = routes
